2788c2322f09bb320a0b03f4604c0ce733ef60b4
Indra <iwahyu@student.telkomuniversity.ac.id>
2022-11-17 11:07:31 +0700

    farm groups

---
+++ app/Http/Controllers/FarmGroupController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FarmGroup;
+use Illuminate\Http\Request;
+
+class FarmGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FarmGroup  $farmGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FarmGroup $farmGroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FarmGroup  $farmGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FarmGroup $farmGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FarmGroup  $farmGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FarmGroup $farmGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FarmGroup  $farmGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FarmGroup $farmGroup)
+    {
+        //
+    }
+}
