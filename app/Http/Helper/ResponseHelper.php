a9652b9158d58f173ad8f25b099e3b2aa868a75b
Indra <iwahyu@student.telkomuniversity.ac.id>
2022-11-23 17:40:05 +0700

    helper

---
+++ app/Http/Helper/ResponseHelper.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Helper;
+
+class ResponseHelper {
+    public static function success($message, $data = null) {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data
+        ]);
+    }
+
+    public static function error($message) {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+        ]);
+    }
+}
