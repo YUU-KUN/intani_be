4435d2de23899f8615c3d93106564978361f93ff
Indra <iwahyu@student.telkomuniversity.ac.id>
2022-11-23 17:38:50 +0700

    api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BankController;
+use App\Http\Controllers\FarmGroupController;
+use App\Http\Controllers\InvestmentController;
+use App\Http\Controllers\InvestorInvestmentController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,38 +19,45 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::middleware(['cors', 'json.response'])->group(function () {
     // Authentication
-    Route::post('/login', [AuthController::class, 'login']); // Login
-    Route::post('/register/company', [AuthController::class, 'registerCompany']); // Register Company
-    Route::post('/register/job_finder', [AuthController::class, 'registerJobFinder']); // Register Job Finder
+    Route::post('login', [AuthController::class, 'login']); // Login
+    Route::post('register', [AuthController::class, 'register']); // Register
     
     // PUBLIC CAN ACCESS
-    // Route::get('job-vacancy/{id}', [JobVacancyController::class, 'show']); // Get Detail Job Vacancy
-    // Route::get('job-training/{id}', [JobTrainingController::class, 'show']); // Get Detail Job Training
+    Route::get('banks', [BankController::class, 'index']); // Get All Banks
+    Route::get('farm_groups', [FarmGroupController::class, 'index']); // Get All Farm Groups
     
     // All with token can access
     Route::middleware('auth:api')->group(function() {
-        // Route::get('user', [AuthController::class, 'getProfile']); // Get User Profile
-        // Route::post('user', [AuthController::class, 'updateProfile']); // Get User Profile
-        // Route::get('my-vacancy-applications', [JobVacancyApplicationController::class, 'index']); // Get User Profile
-        // Route::resource('job-vacancy', JobVacancyController::class); // Job Vacancy
+        Route::get('profile', [AuthController::class, 'profile']); // Get Profile
+        Route::get('farmer_investments', [InvestmentController::class, 'getFarmerInvestment']); // Get Farmer Investment
+        Route::get('investor_investments/{investment_id}', [InvestmentController::class, 'getDetailInvestment']); // Get Investment Detail
+        Route::resource('investments', InvestmentController::class);
     });
-
+    
     // Only Farmer can access
     Route::middleware(['auth:api', 'api.farmer'])->group(function () {
-        // Route::get('company/job_vacancy', [JobVacancyController::class, 'getCompanyJobVacancy']); // Get Conmpany Job Vacancy
-        // Route::resource('company', CompanyController::class); // Company
+        Route::post('investments', [InvestmentController::class, 'store']); // Create Farmer Investment
+        Route::get('farmer_pending_investment', [InvestmentController::class, 'GetFarmerPendingInvestment']); // Get Farmer Pending Investment
+        Route::get('farmer_accepted_investment', [InvestmentController::class, 'getFarmerAcceptedInvestment']); // Get Farmer Accepted Investment
+        Route::get('farmer_completed_investment', [InvestmentController::class, 'getFarmerCompletedInvestment']); // Get Farmer Accepted Investment
+        Route::get('farmer_history_investment', [InvestmentController::class, 'getFarmerHistoryInvestment']); // Get Farmer History Investment
+        Route::post('farmer_finish_investment/{investment_id}', [InvestmentController::class, 'farmerFinishInvestment']); // Farmer FInish Investment
+        
+        Route::get('profit-sharing', [InvestmentController::class, 'getProfitSharing']); // Get Farmer Profit Sharing
+        Route::put('profit-sharing', [InvestmentController::class, 'updateIncome']); // Get Farmer Profit Sharing
+
+        Route::post('profit-sharing', [InvestmentController::class, 'proceedProfitSharing']); // Get Farmer Profit Sharing
+        
+        Route::put('accept_investment/{investment_id}', [InvestmentController::class, 'acceptInvestment']); // Accept Investor Investment
     });
 
     // Only Investor can access
     Route::middleware(['auth:api', 'api.investor'])->group(function () {
-        // Route::resource('job-finder', JobFinderController::class); // Job Finder
-        // Route::post('apply-job-vacancy', [JobVacancyController::class, 'apply']); // Get User Profile
-        // Route::post('apply-job-training', [JobTrainingController::class, 'apply']); // Get User Profile
+        Route::resource('investor_investments', InvestorInvestmentController::class);
+        Route::get('investor_investments', [InvestmentController::class, 'getInvestorInvestment']); // Get Investor Investment
+        Route::get('available_investments', [InvestmentController::class, 'getAvailableInvestment']); // Get Available Investor Investment
+        Route::post('pay_investment', [InvestmentController::class, 'PayInvestment']); // Pay Investor Investment
     });
 });
