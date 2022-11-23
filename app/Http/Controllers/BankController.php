303eb51ab52bb009d74e3b2cb6cdc7ffdde21254
Indra <iwahyu@student.telkomuniversity.ac.id>
2022-11-23 17:42:19 +0700

    bank

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Bank;
 use Illuminate\Http\Request;
+use App\Http\Helper\ResponseHelper;
 
 class BankController extends Controller
 {
@@ -14,7 +15,8 @@ class BankController extends Controller
      */
     public function index()
     {
-        //
+        $banks = Bank::all();
+        return ResponseHelper::success('Success mendapatkan data semua bank', $banks);
     }
 
     /**
