60abb990c4d978cf2d937695d5e16c3455b4d3b4
Indra <iwahyu@student.telkomuniversity.ac.id>
2022-11-23 17:40:32 +0700

    userbank

--- app/Http/Controllers/UserBankController.php
+++ app/Http/Controllers/UserBankController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\UserBank;
 use Illuminate\Http\Request;
+use App\Http\Helper\ResponseHelper;
 
 class UserBankController extends Controller
 {
--- app/Models/UserBank.php
+++ app/Models/UserBank.php
@@ -10,5 +10,20 @@ class UserBank extends Model
 {
     use HasFactory, Uuids;
     protected $table = 'user_banks';
-    protected $fillable = ['user_id', 'bank_id', 'account_name', 'bank_number'];
+    protected $fillable = ['user_id', 'bank_id', 'account_name', 'account_number'];
+
+    public function User()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function Bank()
+    {
+        return $this->belongsTo(Bank::class, 'bank_id');
+    }
+
+    public function getUserAttribute()
+    {
+        return $this->User()->get();
+    }
 }
